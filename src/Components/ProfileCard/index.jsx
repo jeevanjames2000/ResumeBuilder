027c6c8b0463eb5a07f066ec6edd8996a37f0936
Jeevan <Jeevanjames2000@gmail.com>
2023-12-09 23:22:22 +0530

    dashboard ui

---
+++ src/Components/ProfileCard/index.jsx
@@ -0,0 +1,52 @@
+import { Card, CardContent, CardHeader, Grid, TextField } from '@mui/material'
+import React from 'react'
+
+export default function Profile() {
+    return (
+        <>
+            <form autoComplete="off" noValidate>
+
+                <Card>
+                    <CardHeader title="Add Profile Details" />
+                    <CardContent>
+                        <Grid container >
+                            <Grid item container xs={12} spacing={4}>
+                                <Grid item xs={12}>
+                                    <TextField
+                                        fullWidth
+                                        name="firstName"
+                                        label="First Name"
+                                        variant="outlined"
+                                    />
+                                </Grid>
+                                <Grid item xs={12}>
+                                    <TextField
+                                        fullWidth
+                                        name="secondName"
+                                        label="Second Name"
+                                        variant="outlined"
+                                    />
+                                </Grid>
+                                <Grid item xs={12} >
+                                    <TextField
+                                        fullWidth
+                                        name="phone"
+                                        label="Phone Number"
+                                        variant="outlined"
+                                    />
+                                </Grid>  <Grid item xs={12}>
+                                    <TextField
+                                        fullWidth
+                                        name="address"
+                                        label="Address"
+                                        variant="outlined"
+                                    />
+                                </Grid>
+                            </Grid>
+                        </Grid>
+                    </CardContent>
+                </Card>
+            </form>
+        </>
+    )
+}
---
+++ src/Components/Stepper.jsx
@@ -0,0 +1,150 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import { Grid, Step, StepButton, Stepper } from '@mui/material';
+import Profile from './ProfileCard';
+
+const steps = [
+    {
+        id: 1,
+        title: 'Profile',
+        content: <Profile />,
+    },
+    {
+        id: 2,
+        title: 'Education',
+        content: 'Second-content',
+    },
+    {
+        id: 3,
+        title: 'Skills',
+        content: 'Last-content',
+    },
+    {
+        id: 4,
+        title: 'Projects',
+        content: 'Last-content',
+    },
+    {
+        id: 5,
+        title: 'Experience',
+        content: 'Last-content',
+    },
+    {
+        id: 6,
+        title: 'Social Links',
+        content: 'Last-content',
+    },
+];
+
+export default function ResumeSteps() {
+    const [activeStep, setActiveStep] = React.useState(0);
+    const [completed, setCompleted] = React.useState({});
+
+    const totalSteps = () => {
+        return steps.length;
+    };
+
+    const completedSteps = () => {
+        return Object.keys(completed).length;
+    };
+
+    const isLastStep = () => {
+        return activeStep === totalSteps() - 1;
+    };
+
+    const allStepsCompleted = () => {
+        return completedSteps() === totalSteps();
+    };
+
+    const handleNext = () => {
+        const newActiveStep =
+            isLastStep() && !allStepsCompleted()
+                ? // It's the last step, but not all steps have been completed,
+                // find the first step that has been completed
+                steps.findIndex((step, i) => !(i in completed))
+                : activeStep + 1;
+        setActiveStep(newActiveStep);
+    };
+
+    const handleBack = () => {
+        setActiveStep((prevActiveStep) => prevActiveStep - 1);
+    };
+
+    const handleStep = (step) => () => {
+        setActiveStep(step);
+    };
+
+    const handleComplete = () => {
+        const newCompleted = completed;
+        newCompleted[activeStep] = true;
+        setCompleted(newCompleted);
+        handleNext();
+    };
+
+    const handleReset = () => {
+        setActiveStep(0);
+        setCompleted({});
+    };
+
+    return (
+        <Box sx={{ width: '100%' }}>
+            <Stepper nonLinear activeStep={activeStep}>
+                {steps.map((label, index) => (
+                    <Step key={label.id} completed={completed[index]}>
+                        <StepButton color="inherit" onClick={handleStep(index)}>
+                            {label.title}
+                        </StepButton>
+                    </Step>
+                ))}
+            </Stepper>
+            <div>
+                {allStepsCompleted() ? (
+                    <React.Fragment>
+                        <Typography sx={{ mt: 2, mb: 1 }}>
+                            All steps completed - you&apos;re finished
+                        </Typography>
+                        <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
+                            <Box sx={{ flex: '1 1 auto' }} />
+                            <Button onClick={handleReset}>Reset</Button>
+                        </Box>
+                    </React.Fragment>
+                ) : (
+                    <React.Fragment>
+
+                        <div >{steps[activeStep].content}</div>
+
+                        <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
+                            <Button
+                                color="inherit"
+                                disabled={activeStep === 0}
+                                onClick={handleBack}
+                                sx={{ mr: 1 }}
+                            >
+                                Back
+                            </Button>
+                            <Box sx={{ flex: '1 1 auto' }} />
+                            <Button onClick={handleNext} sx={{ mr: 1 }}>
+                                Next
+                            </Button>
+                            {activeStep !== steps.length &&
+                                (completed[activeStep] ? (
+                                    <Typography variant="caption" sx={{ display: 'inline-block' }}>
+                                        Step {activeStep + 1} already completed
+                                    </Typography>
+                                ) : (
+                                    <Button onClick={handleComplete}>
+                                        {completedSteps() === totalSteps() - 1
+                                            ? 'Finish'
+                                            : 'Complete Step'}
+                                    </Button>
+                                ))}
+                        </Box>
+                    </React.Fragment>
+                )}
+            </div>
+        </Box>
+    );
+}
